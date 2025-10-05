9e0be22de492b57706fed8d4d0bb0d96928219c6
Fuminiton <fumi_8823@icloud.com>
2025-10-05 23:11:47 +0900

    add: parser and code generator

--- ccc.c
+++ ccc.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+/* struct / union / enum */
 // Token Kinds
 typedef enum {
   TK_RESERVED,     // Symbol
@@ -13,21 +13,81 @@ typedef enum {
   TK_EOF,          // End-of-input token
 } TokenKind;
 
+// AST Node Kinds
+typedef enum {
+  ND_ADD,          // +
+  ND_SUB,          // -
+  ND_MUL,          // *
+  ND_DIV,          // /
+  ND_NUM,          // number
+} NodeKind;
+
+/* typedef */
 typedef struct Token Token;
+typedef struct Node Node;
 
 // Token Type
 struct Token {
-  TokenKind kind;  // Token type
+  TokenKind kind;  // Token struct
   Token *next;     // Next input token
   int val;         // Numeric value when kind is TK_NUM
   char *str;       // Token String
 };
 
+// Node Type
+struct Node {
+  NodeKind kind;   // Node struct
+  Node *lhs;       // left-hand side
+  Node *rhs;       // right-hand side
+  int val;
+};
+
+
+/* global variables */
 // Input program
 char *user_input;
 
 // Token currently under consideration
 Token *token;
+
+
+/* define prottype */
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char op);
+void expect(char op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *current_token, char *str);
+Token *tokenize();
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *primary();
+Node *mul();
+Node *expr();
+void gen(Node *node);
+
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "The number of arguments is incorrect\n");
+        return 1;
+    }
+
+    user_input = argv[1];
+    token = tokenize();
+    Node *node = expr();
+
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+    
+    gen(node);
+
+    printf("  pop rax\n");
+    printf("  ret\n");
+    return 0;
+}
 
 // Function to report an error
 // Takes the same arguments as printf
@@ -38,7 +98,6 @@ void error(char *fmt, ...) {
   fprintf(stderr, "\n");
   exit(1);
 }
-
 
 // Report the error position
 void error_at(char *loc, char *fmt, ...) {
@@ -114,7 +173,7 @@ Token *tokenize() {
     }
 
     // Punctuator
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-*/()", *p)) {
       current_token = new_token(TK_RESERVED, current_token, p++);
       continue;
     }
@@ -128,36 +187,93 @@ Token *tokenize() {
       continue;
     }
 
-    error_at(p, "expected a number");
+    error_at(p, "invalid token");
   }
   new_token(TK_EOF, current_token, p);
   return head.next;
 }
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "The number of arguments is incorrect\n");
-        return 1;
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+      continue;
     }
-
-    user_input = argv[1];
-    token = tokenize();
-
-    printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
-    printf("main:\n");
-    
-    printf("  mov rax, %d\n", expect_number());
-
-    while (!at_eof()) {
-      if (consume('+')) {
-        printf("  add rax, %d\n", expect_number());
-        continue;
-      }
-      expect('-');
-      printf("  sub rax, %d\n", expect_number());
+    if (consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+      continue;
     }
+    return node;
+  }
+}
 
-    printf("  ret\n");
-    return 0;
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+      continue;
+    }
+    if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+      continue;
+    }
+    return node;
+  }
+}
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+  printf("  push rax\n");
 }
